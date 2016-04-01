61ef303c68bae9b5da46eea19eb940e5ec60ee0a
huiwq1990 <huiwq1990@163.com>
2016-04-01 16:47:20 +0800

    sql

--- msg-notify/src/main/resources/sql/msg_notify.sql
+++ msg-notify/src/main/resources/sql/msg_notify.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50621
 File Encoding         : 65001
 
-Date: 2016-03-28 20:03:06
+Date: 2016-04-01 16:46:01
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -20,33 +20,33 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 DROP TABLE IF EXISTS `msg_notify`;
 CREATE TABLE `msg_notify` (
-  `id` bigint(20) DEFAULT NULL COMMENT '主键',
+  `id` bigint(20) NOT NULL COMMENT '主键',
   `content` varchar(1000) DEFAULT NULL COMMENT '消息内容',
   `type` int(11) DEFAULT NULL COMMENT '消息的类型，1: 公告 Announce，2: 提醒 Remind，3：信息 Message',
-  `target` int(11) DEFAULT NULL,
-  `targetType` varchar(100) DEFAULT NULL,
+  `target` bigint(11) DEFAULT NULL,
+  `target_type` varchar(100) DEFAULT NULL,
   `action` varchar(100) DEFAULT NULL,
   `sender` bigint(20) DEFAULT NULL,
-  `createTime` datetime DEFAULT NULL
+  `create_time` datetime DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  `del_status` int(11) DEFAULT '0',
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of msg_notify
--- ----------------------------
 
 -- ----------------------------
 -- Table structure for msg_save_remind
 -- ----------------------------
 DROP TABLE IF EXISTS `msg_save_remind`;
 CREATE TABLE `msg_save_remind` (
+  `id` bigint(20) NOT NULL,
   `target` bigint(20) DEFAULT NULL,
-  `targetType` varchar(100) DEFAULT NULL,
-  `sender` bigint(20) DEFAULT NULL
+  `target_type` varchar(100) DEFAULT NULL,
+  `sender` bigint(20) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  `del_status` int(11) DEFAULT '0',
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of msg_save_remind
--- ----------------------------
 
 -- ----------------------------
 -- Table structure for msg_subscription
@@ -55,42 +55,41 @@ DROP TABLE IF EXISTS `msg_subscription`;
 CREATE TABLE `msg_subscription` (
   `id` bigint(20) NOT NULL,
   `target` bigint(20) DEFAULT NULL,
-  `targetType` varchar(100) DEFAULT NULL,
+  `target_type` varchar(100) DEFAULT NULL,
   `action` varchar(100) DEFAULT NULL,
-  `user` bigint(20) DEFAULT NULL,
+  `user_id` bigint(20) DEFAULT NULL,
   `create_time` datetime DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  `del_status` int(11) DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of msg_subscription
--- ----------------------------
 
 -- ----------------------------
 -- Table structure for msg_subscription_config
 -- ----------------------------
 DROP TABLE IF EXISTS `msg_subscription_config`;
 CREATE TABLE `msg_subscription_config` (
+  `id` bigint(20) NOT NULL,
+  `user_id` bigint(20) DEFAULT NULL,
   `action` varchar(1000) DEFAULT NULL,
-  `user` bigint(20) DEFAULT NULL
+  `create_time` datetime DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  `del_status` int(11) DEFAULT '0',
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of msg_subscription_config
--- ----------------------------
 
 -- ----------------------------
 -- Table structure for msg_user_notify
 -- ----------------------------
 DROP TABLE IF EXISTS `msg_user_notify`;
 CREATE TABLE `msg_user_notify` (
-  `id` bigint(20) DEFAULT NULL,
-  `isRead` int(11) DEFAULT NULL,
-  `user` bigint(20) DEFAULT NULL,
-  `notify` bigint(20) DEFAULT NULL,
-  `createTime` datetime DEFAULT NULL
+  `id` bigint(20) NOT NULL,
+  `is_read` int(11) DEFAULT NULL,
+  `user_id` bigint(20) DEFAULT NULL,
+  `notify_id` bigint(20) DEFAULT NULL,
+  `notify_type` int(255) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  `del_status` int(11) DEFAULT '0',
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of msg_user_notify
--- ----------------------------
