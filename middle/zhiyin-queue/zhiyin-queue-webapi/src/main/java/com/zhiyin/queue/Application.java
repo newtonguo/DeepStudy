14e13f7a66f292ccf38bfc818102b3d49617b7d4
huiwq1990 <huiwq1990@163.com>
2016-04-20 00:13:26 +0800

    change to web

--- middle/zhiyin-queue/zhiyin-queue-webapi/src/main/java/com/zhiyin/queue/Application.java
+++ middle/zhiyin-queue/zhiyin-queue-webapi/src/main/java/com/zhiyin/queue/Application.java
@@ -2,14 +2,21 @@ package com.zhiyin.queue;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @ComponentScan
-public class Application extends WebMvcConfigurerAdapter {
+public class Application extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
 
