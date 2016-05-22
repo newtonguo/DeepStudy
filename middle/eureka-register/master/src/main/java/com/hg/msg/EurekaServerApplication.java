da1d5f9d1ea28523a019cc9c2059611844b0d22c
huiwq1990 <huiwq1990@163.com>
2016-05-22 23:51:42 +0800

    admin client

--- middle/eureka-register/master/src/main/java/com/hg/msg/EurekaServerApplication.java
+++ middle/eureka-register/master/src/main/java/com/hg/msg/EurekaServerApplication.java
@@ -2,17 +2,21 @@ package com.hg.msg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaServerApplication {
+public class EurekaServerApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
-        // Tell server to look for registration.properties or registration.yml
-        //System.setProperty("spring.config.name", "registration-server");
-
         SpringApplication.run(EurekaServerApplication.class, args);
     }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(EurekaServerApplication.class);
+    }
+
 }
