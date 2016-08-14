eaede275fecfb1882189e3d16b885d0ce272d2b1
huiwq1990 <huiwq1990@163.com>
2016-08-14 13:06:28 +0800

    gateway

--- spring-security-oauth/zy-oauth-server/src/main/java/com/zhiyin/oauth2/oauthserver/OauthServerApplication.java
+++ spring-security-oauth/zy-oauth-server/src/main/java/com/zhiyin/oauth2/oauthserver/OauthServerApplication.java
@@ -3,11 +3,22 @@ package com.zhiyin.oauth2.oauthserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
-//@EnableAutoConfiguration(exclude = {org.activiti.spring.boot.SecurityAutoConfiguration.class})
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class OauthServerApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(OauthServerApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(OauthServerApplication.class, args);
