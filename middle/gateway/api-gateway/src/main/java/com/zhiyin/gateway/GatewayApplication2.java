0844c8acd0de26e038e2dc6bc78d09bbf508757d
huiwq1990 <huiwq1990@163.com>
2016-08-21 12:49:19 +0800

    gate

--- middle/gateway/api-gateway/src/main/java/com/zhiyin/gateway/GatewayApplication2.java
+++ middle/gateway/api-gateway/src/main/java/com/zhiyin/gateway/GatewayApplication2.java
@@ -1,29 +1,29 @@
-package com.zhiyin.gateway;
-
-import com.zhiyin.gateway.filter.AccessFilter2;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
-
-@EnableZuulProxy
-@SpringCloudApplication
-public class GatewayApplication2 extends SpringBootServletInitializer {
-
-    public static void main(String[] args) {
-        SpringApplication.run(GatewayApplication.class, args);
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(GatewayApplication.class);
-    }
-
-    @Bean
-    public AccessFilter2 accessFilter() {
-        return new AccessFilter2();
-    }
-
-}
+//package com.zhiyin.gateway;
+//
+//import com.zhiyin.gateway.filter.AccessFilter2;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.builder.SpringApplicationBuilder;
+//import org.springframework.boot.context.web.SpringBootServletInitializer;
+//import org.springframework.cloud.client.SpringCloudApplication;
+//import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+//import org.springframework.context.annotation.Bean;
+//
+//@EnableZuulProxy
+//@SpringCloudApplication
+//public class GatewayApplication2 extends SpringBootServletInitializer {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(GatewayApplication.class, args);
+//    }
+//
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//        return application.sources(GatewayApplication.class);
+//    }
+//
+//    @Bean
+//    public AccessFilter2 accessFilter() {
+//        return new AccessFilter2();
+//    }
+//
+//}
