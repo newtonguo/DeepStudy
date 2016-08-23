7ceef49ac05397ee3608e624cbb2e08ec9175ac0
huiwq1990 <huiwq1990@163.com>
2016-08-24 06:56:13 +0800

    new

--- middle/gateway/service-B/src/main/java/com/zhiyin/AServiceApplication.java
+++ middle/gateway/service-B/src/main/java/com/zhiyin/AServiceApplication.java
@@ -1,15 +1,21 @@
 package com.zhiyin;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class AServiceApplication {
+public class AServiceApplication  extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
-        new SpringApplicationBuilder(AServiceApplication.class).web(true).run(args);
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AServiceApplication.class);
     }
 
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(AServiceApplication.class, args);
+    }
 }
